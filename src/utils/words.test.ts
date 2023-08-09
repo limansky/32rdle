df3036c9980cd12cc27619b1718ea8e672735a45
Mike Limansky <mike.limansky@gmail.com>
2023-08-09 16:02:39 +0300

    Make all tests pass

--- src/utils/words.test.ts
+++ src/utils/words.test.ts
@@ -3,9 +3,11 @@ import { wordStatus } from './words';
 import { LetterState } from '../components/Letter';
 
 test.each([
-  ['пакля', 'кабак', [LetterState.Miss, LetterState.Guess, LetterState.WrongPosition, LetterState.Miss, LetterState.Miss]],
+  ['пакля', 'кабак', [LetterState.WrongPosition, LetterState.Guess, LetterState.Miss, LetterState.Miss, LetterState.Miss]],
+  ['кабак', 'пакля', [LetterState.Miss, LetterState.Guess, LetterState.WrongPosition, LetterState.Miss, LetterState.Miss]],
   ['кабак', 'папка', [LetterState.Miss, LetterState.Guess, LetterState.Miss, LetterState.WrongPosition, LetterState.WrongPosition]],
-  ['пакля', 'папка', [LetterState.Guess, LetterState.Guess, LetterState.Miss, LetterState.WrongPosition, LetterState.Miss]]
+  ['пакля', 'папка', [LetterState.Guess, LetterState.Guess, LetterState.Miss, LetterState.WrongPosition, LetterState.Miss]],
+  ['шапка', 'папка', [LetterState.Miss, LetterState.Guess, LetterState.Guess, LetterState.Guess, LetterState.Guess]]
 ])('wordStatus(%s, %s) == %o', (word, guess, expected) => {
   expect(wordStatus(word, guess)).toStrictEqual(expected);
 });
--- src/utils/words.ts
+++ src/utils/words.ts
@@ -1,7 +1,27 @@
 import { LetterState } from "../components/Letter";
 
 export function wordStatus(word: string, guess: string): Array<LetterState> {
-  return Array.from(word).map((c, i) => {
-    return guess[i] === c ? LetterState.Guess : guess.indexOf(c) !== -1 ? LetterState.WrongPosition : LetterState.Miss;
+  const len = guess.length;
+  var result = Array(len).fill(LetterState.Miss);
+  var ref = Array(len).fill(-1);
+
+  Array.from(guess).forEach((c, i) => {
+    if (word[i] == c) {
+      result[i] = LetterState.Guess;
+      if (ref[i] != -1) {
+        result[ref[i]] = LetterState.Miss;
+      }
+      ref[i] = i;
+    } else {
+      for (let j = 0; j < len; j++) {
+        if (word[j] == c && ref[j] == -1) {
+          ref[j] = i;
+          result[i] = LetterState.WrongPosition;
+          break;
+        }
+      }
+    }
   });
+
+  return result;
 }
