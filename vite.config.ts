f2d16727c3aa961a14e34b50024065e977409c97
Mike Limansky <mike.limansky@gmail.com>
2023-12-23 22:03:10 +0300

    Use eslint vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslintPlugin()],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, '/src'),
