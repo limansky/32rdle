1ab0b73a8a9530fc3640a786c6383bc9820df9a1
Mike Limansky <mike.limansky@gmail.com>
2024-05-01 13:30:04 +0300

    Migrate to eslint flat configs, update eslint to 9.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
 import path from 'path'
 
 // https://vitejs.dev/config/
