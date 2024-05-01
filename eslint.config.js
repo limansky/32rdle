5a5f409d4535928a8c80851ba535a3e399e57551
Mike Limansky <mike.limansky@gmail.com>
2024-05-01 13:48:55 +0300

    Activate more eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,18 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
 
 export default tseslint.config(
-   eslint.configs.recommended,
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-hooks": eslintPluginReactHooks
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    }
+  }
 );
